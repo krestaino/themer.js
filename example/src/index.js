076976e6c1369cfbed566c4b30487041791faf29
krestaino <kevinrestaino@gmail.com>
2019-05-04 20:41:11 -0500

    chore: cleanup example folder

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Themer from "react-themer.js";
+import "./index.css";
+
+class App extends Component {
+  state = {
+    active: "auto",
+    themes: ["auto", "system", "light", "dark", "custom"]
+  };
+
+  render() {
+    const { active, themes } = this.state;
+
+    return (
+      <Themer mode={active}>
+        <main>
+          <div>
+            {themes.map(theme => (
+              <button
+                className={theme === active ? "active" : ""}
+                key={theme}
+                onClick={() => this.setState({ active: theme })}
+              >
+                {theme}
+              </button>
+            ))}
+          </div>
+        </main>
+      </Themer>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
