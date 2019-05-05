ba30369f9c7f0ad780e8e403d0667b7868c821a3
krestaino <kevinrestaino@gmail.com>
2019-05-05 18:03:32 -0500

    chore: add example build files to repo

---
+++ example/build/precache-manifest.d9faf84983a439522c0efb54b47c28b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63816a888a6c8a73944bfd48391c9dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a91bb7d2027e7a5c6da6",
+    "url": "/static/css/main.534af9e8.chunk.css"
+  },
+  {
+    "revision": "999f5dc77891147bef52",
+    "url": "/static/js/2.144b066e.chunk.js"
+  },
+  {
+    "revision": "a91bb7d2027e7a5c6da6",
+    "url": "/static/js/main.c8444f0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfcc531783a4d4803d0564c6eae51813",
+    "url": "/static/media/README.cfcc5317.md"
+  }
+]);
