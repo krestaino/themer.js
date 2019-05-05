bf55cb98615527ad6ba3cc49e53f9c3d54aef7fb
krestaino <kevinrestaino@gmail.com>
2019-05-05 00:54:20 -0500

    feat: example style tweaks

--- example/src/index.js
+++ example/src/index.js
@@ -22,28 +22,71 @@ class App extends Component {
       .then(text => this.setState({ markdown: text }));
   }
 
+  getEmoji() {
+    switch (this.state.active) {
+      case "auto":
+        return "🌗";
+      case "system":
+        return "💻";
+      case "light":
+        return "☀️";
+      case "dark":
+        return "🌑";
+      case "custom":
+        return "🎨";
+      default:
+        return "🌗";
+    }
+  }
+
   render() {
     const { active, themes } = this.state;
 
     return (
       <Themer theme={active}>
         <main>
-          <div>
-            <h1>Themer.js</h1>
-            <a href="https://github.com/krestaino/themer.js">
-              github.com/krestaino/themer.js
-            </a>
-            {themes.map(theme => (
-              <button
-                className={theme === active ? "active" : ""}
-                key={theme}
-                onClick={() => this.setState({ active: theme })}
+          <section>
+            <header>
+              <div className="meta">
+                <h1>Themer.js</h1>
+                <a href="https://github.com/krestaino/themer.js">
+                  github.com/krestaino/themer.js
+                </a>
+                <p>
+                  {this.getEmoji()} Automatically switch between dark and light
+                  themes at sunset and sunrise using the user's location.
+                </p>
+              </div>
+              <div className="buttons">
+                {themes.map(theme => (
+                  <button
+                    className={theme === active ? "active" : ""}
+                    key={theme}
+                    onClick={() => this.setState({ active: theme })}
+                  >
+                    {theme}
+                  </button>
+                ))}
+              </div>
+            </header>
+            <div className="readme">
+              <ReactMarkdown source={this.state.markdown} />
+            </div>
+            <footer>
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
               >
-                {theme}
-              </button>
-            ))}
-            <ReactMarkdown source={this.state.markdown} />
-          </div>
+                <path
+                  fill="currentColor"
+                  d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"
+                />
+              </svg>
+            </footer>
+          </section>
         </main>
       </Themer>
     );
