21179921d5b8265c4ca77cfd1d2cd7908be52884
krestaino <kevinrestaino@gmail.com>
2019-05-07 00:48:22 -0500

    chore: add build files

---
+++ example/build/precache-manifest.4478e22479994a53bb6c0df0328d4298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "576458dffd2504f1aa04ec3bca4a307d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efdee684fdff5dca7aad",
+    "url": "/static/css/main.171de0be.chunk.css"
+  },
+  {
+    "revision": "4bdfcb861f8f169a2096",
+    "url": "/static/js/2.26c9791f.chunk.js"
+  },
+  {
+    "revision": "efdee684fdff5dca7aad",
+    "url": "/static/js/main.83bce67f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f08379720f441f265099e307f4097c41",
+    "url": "/static/media/README.f0837972.md"
+  }
+]);
